b0e618d1dba1174183aa78a0fa8e604ba2a9ced6
JSJBDEV <acrodynasty@gmail.com>
2024-02-29 02:09:25 +0000

    Added player mixin to store data better

---
+++ src/main/java/ace/actually/valkyrienrelogs/PlayerShipAnchorAccessor.java
@@ -0,0 +1,12 @@
+package ace.actually.valkyrienrelogs;
+
+import net.minecraft.util.math.BlockPos;
+
+public interface PlayerShipAnchorAccessor {
+
+    BlockPos relativeShipPosition();
+    void setRelativeShipPosition(BlockPos p);
+    boolean loggedOffOnShip();
+
+    void setLoggedOffOnShip(boolean d);
+}
---
+++ src/main/java/ace/actually/valkyrienrelogs/mixin/PlayerMixin.java
@@ -0,0 +1,70 @@
+package ace.actually.valkyrienrelogs.mixin;
+
+import ace.actually.valkyrienrelogs.PlayerShipAnchorAccessor;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.data.DataTracker;
+import net.minecraft.entity.data.TrackedData;
+import net.minecraft.entity.data.TrackedDataHandlerRegistry;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.nbt.NbtCompound;
+import net.minecraft.nbt.NbtHelper;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(PlayerEntity.class)
+public abstract class PlayerMixin extends LivingEntity implements PlayerShipAnchorAccessor {
+
+	private static final TrackedData<NbtCompound> ANCHOR_DATA = DataTracker.registerData(PlayerEntity.class, TrackedDataHandlerRegistry.NBT_COMPOUND);
+
+	protected PlayerMixin(EntityType<? extends LivingEntity> entityType, World world) {
+		super(entityType, world);
+	}
+
+	@Override
+	public BlockPos relativeShipPosition() {
+		return NbtHelper.toBlockPos(dataTracker.get(ANCHOR_DATA).getCompound("relativeShipPosition"));
+	}
+
+	@Override
+	public boolean loggedOffOnShip() {
+		return dataTracker.get(ANCHOR_DATA).getBoolean("loggedOffOnShip");
+	}
+
+	@Override
+	public void setRelativeShipPosition(BlockPos p) {
+		NbtCompound tag = dataTracker.get(ANCHOR_DATA);
+		tag.put("relativeShipPosition",NbtHelper.fromBlockPos(p));
+		dataTracker.set(ANCHOR_DATA,tag);
+	}
+
+	@Override
+	public void setLoggedOffOnShip(boolean d) {
+		NbtCompound tag = dataTracker.get(ANCHOR_DATA);
+		tag.putBoolean("loggedOffOnShip",d);
+		dataTracker.set(ANCHOR_DATA,tag);
+	}
+
+	@Inject(method = "readCustomDataFromNbt", at = @At("TAIL"))
+	public void readCustomDataFromNbt(NbtCompound nbt, CallbackInfo ci) {
+		dataTracker.set(ANCHOR_DATA,nbt.getCompound("anchor_data"));
+
+	}
+
+	@Inject(method = "writeCustomDataToNbt", at = @At("TAIL"))
+	public void writeCustomDataToNbt(NbtCompound nbt, CallbackInfo ci) {
+		nbt.put("anchor_data",dataTracker.get(ANCHOR_DATA));
+
+	}
+
+	@Inject(method = "initDataTracker", at = @At("TAIL"))
+	protected void initDataTracker(CallbackInfo ci) {
+		NbtCompound a = new NbtCompound();
+		dataTracker.startTracking(ANCHOR_DATA,a.copy());
+
+	}
+}
